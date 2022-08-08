93ffda10ae5cbc0eeeea6e4519ab326a63f4cd66
Luigi <luigimarqs@gmail.com>
2022-08-08 04:21:39 -0300

    cu

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "arm_shape" {
 
 variable "arm_image" {
     description = "To grab image ID use the command commented below "
-    //oci compute image list --operating-system Canonical Ubuntu 
+    //oci compute image list --operating-system 'Canonical Ubuntu' --compartment-id ocid1.compartment.oc1..aaaaaaaac5kc6qfkvneoqlxkk5rgnwog57bcdeah2kmpyrjye5yjrdelz52a --shape VM.Standard.A1.Flex
     // https://docs.oracle.com/en-us/iaas/tools/oci-cli/2.17.0/oci_cli_docs/cmdref/compute/image/list.html
-    default = "vocid1.image.oc1.us-sanjose-1.aaaaaaaawu6gqwohgcs7ggd5xx64pw5ebl7ij5gijgnbxnhqnqrdc2kihqlq"
+    default = "ocid1.image.oc1.us-sanjose-1.aaaaaaaa3prdpji4w36ni2blanuvbg2w3byebux3vqvtd6yc2uiryy5ge33a"
 }
