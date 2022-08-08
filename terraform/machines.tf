fe7e93e7d5055df0481bd1bea3d1fa4667c46043
Luigi <luigimarqs@gmail.com>
2022-08-08 04:11:45 -0300

    oh god please noaaaaa

--- terraform/machines.tf
+++ terraform/machines.tf
@@ -37,34 +37,7 @@ resource "oci_core_instance" "arm_01" {
   }
 }
 
-resource "oci_core_instance" "arm_02" {
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  compartment_id      = var.compartment_ocid
-  display_name        = "tank02"
-  shape               = var.arm_shape
 
-  shape_config {
-    ocpus = 2
-    memory_in_gbs = "12"
-  }
-
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.gatonet_subnet.id
-    display_name     = "primaryvnic"
-    assign_public_ip = true
-    hostname_label   = "tank01"
-  }
-
-  source_details {
-    source_type = "image"
-    source_id   = var.arm_image
-    boot_volume_size_in_gbs = 65
-  }
-
-  metadata = {
-    ssh_authorized_keys = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.public_key_openssh
-  }
-}
 
 // Server SSH Key
 resource "tls_private_key" "compute_ssh_key" {
